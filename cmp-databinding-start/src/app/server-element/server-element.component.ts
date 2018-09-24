704413ff6b7103d6c730da1c846033a2a0954348
Jared Eklin <jaredeklin@gmail.com>
2018-09-24 11:14:11 -0600

    Add databinding assignment

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
 
   @Input() element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called')
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called');
+    
+  }
+
+  ngOnChanges(changes: simpleChanges) {
+    console.log('ngOnChanges called')
+    console.log(changes);
+    
   }
 
 }
