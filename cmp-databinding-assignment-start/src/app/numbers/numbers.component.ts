1ddee99c01e2850befe39cae3d47e7fc9fed4737
Jared Eklin <jaredeklin@gmail.com>
2018-09-24 15:41:41 -0600

    Refactor assignment to include a number container

---
+++ cmp-databinding-assignment-start/src/app/numbers/numbers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers',
+  templateUrl: './numbers.component.html',
+  styleUrls: ['./numbers.component.css']
+})
+export class NumbersComponent implements OnInit {
+  @Input() number: number;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
