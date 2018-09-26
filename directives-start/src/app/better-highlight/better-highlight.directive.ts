493a9cc3406640aabc7542651fb0fa3193025696
Jared Eklin <jaredeklin@gmail.com>
2018-09-26 14:25:43 -0600

    Add hover using host binding

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,11 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+
+  @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
@@ -12,11 +14,13 @@ export class BetterHighlightDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color", "yellow");
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", "yellow");
+    this.backgroundColor = 'yellow'
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, "background-color", "transparent");
+    // this.renderer.setStyle(this.elRef.nativeElement, "background-color", "transparent");
+    this.backgroundColor = 'unset'
   }
 
 }
