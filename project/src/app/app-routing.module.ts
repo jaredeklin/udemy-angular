007d7a590c1d2503cf833d71229b68a184c329f8
Jared Eklin <jaredeklin@gmail.com>
2018-10-05 08:56:18 -0600

    Add routes for new recipe and edit recipe

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,20 +1,27 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { HeaderComponent } from "./header/header.component";
-import { AppComponent } from "./app.component";
-import { RecipiesComponent } from "./recipies/recipies.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { RecipiesStartComponent } from "./recipies/recipies-start/recipies-start.component";
-import { RecipeDetailComponent } from "./recipies/recipe-detail/recipe-detail.component";
+import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipiesStartComponent } from './recipies/recipies-start/recipies-start.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipies/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipies', pathMatch: 'full' },
-  { path: 'recipies', component: RecipiesComponent, children: [
-    { path: '', component: RecipiesStartComponent, pathMatch: 'full' },
-    { path: ':id', component: RecipeDetailComponent }
-  ] },
-  { path: 'shopping-list', component: ShoppingListComponent },
+  {
+    path: 'recipies',
+    component: RecipiesComponent,
+    children: [
+      { path: '', component: RecipiesStartComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit-recipe', component: RecipeEditComponent }
+    ]
+  },
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
