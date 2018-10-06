c114eae405520ce8a57194ff36550e9876825433
Jared Eklin <jaredeklin@gmail.com>
2018-10-06 09:49:42 -0600

    Build observable from scratch

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable} from 'rxjs';
+import { Observer } from 'rxjs/Observer'
+import 'rxjs/Rx';
+
 
 @Component({
   selector: 'app-home',
@@ -10,6 +14,22 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // const myNums = Observable.interval(1000);
+    // myNums.subscribe((number: Number) => {
+    //   console.log(number)
+    // })
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => observer.next('first package'), 2000);
+      setTimeout(() => observer.next('second package'), 4000);
+      setTimeout(() => observer.complete(), 5000);
+    });
+
+    myObservable.subscribe(
+      (data: string) => console.log(data),
+      (error: string) => console.log(error),
+      () => console.log('completed')
+    );
   }
 
 }
