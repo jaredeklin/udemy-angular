a9c0ff644fdf66d737af8185bdba246f7c715ab3
Jared Eklin <jaredeklin@gmail.com>
2018-10-10 17:56:19 -0600

    Add getIngredient and updateIngredient methods to SL service

--- project/src/app/shopping-list/shopping-list.service.ts
+++ project/src/app/shopping-list/shopping-list.service.ts
@@ -1,12 +1,13 @@
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs";
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
-import { Ingredient } from "../shared/ingredient.model";
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({providedIn: 'root'})
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 10),
     new Ingredient('Milk', 2)
@@ -19,5 +20,14 @@ export class ShoppingListService {
 
   getIngredients() {
     return [...this.ingredients];
+  }
+
+  getIngredient(id: number) {
+    return this.ingredients[id];
+  }
+
+  updateIngredients(id: number, newIngredient: Ingredient) {
+    this.ingredients[id] = newIngredient;
+    this.ingredientsChanged.next([...this.ingredients]);
   }
 }
