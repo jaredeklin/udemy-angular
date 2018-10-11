cb7151d60330c3bb94ef16ff3c34213ede76c8bf
Jared Eklin <jaredeklin@gmail.com>
2018-10-11 10:49:49 -0600

    Add servers service and create post request

--- http-start/src/app/servers.service.ts
+++ http-start/src/app/servers.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServersService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  storeServers(servers: any[]) {
+    return this.http.post('https://ngpractice-c71e6.firebaseio.com/data.json', servers);
+  }
 }
